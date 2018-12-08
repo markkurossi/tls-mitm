28c85ab03d2348ac795ab1c12ccd36f85282828b
Markku Rossi <mtr@iki.fi>
2018-12-08 17:27:55 +0200

    Create CA hierarchy with CA and EE certificates.

--- main.go
+++ main.go
@@ -32,17 +32,24 @@ const (
 var config *tls.Config
 
 func main() {
-	keygen := flag.String("keygen", "", "Generate a keypair to the named file.")
-	san := flag.String("san", "", "Subject alternative name for key.")
-	keyFile := flag.String("key", "mitm", "TLS key and certificate.")
+	keygen := flag.Bool("keygen", false, "Generate CA and TLS keys.")
+	certgen := flag.String("certgen", "",
+		"Generate TLS certificates with argument subject alternative name.")
 	listen := flag.String("listen", "", "The address to listen to.")
 	proxy := flag.String("proxy", "", "The address to proxy connections.")
 	flag.Parse()
 
-	if len(*keygen) > 0 {
-		err := makeKey(*keygen, *san)
+	if *keygen {
+		err := makeKeys()
 		if err != nil {
 			log.Fatalf("Failed to create keypair: %s\n", err)
+		}
+		return
+	}
+	if len(*certgen) > 0 {
+		err := makeCert(*certgen)
+		if err != nil {
+			log.Fatalf("Failed to create certificate: %s\n", err)
 		}
 		return
 	}
@@ -55,10 +62,20 @@ func main() {
 		return
 	}
 
-	key, cert, certBytes, err := loadKey(*keyFile)
+	key, err := loadKey("mitm")
 	if err != nil {
-		log.Fatalf("Failed to load key '%s': %s\n", *keyFile, err)
+		log.Fatalf("Failed to load key: %s\n", err)
 	}
+	cert, certBytes, err := loadCert("mitm")
+	if err != nil {
+		log.Fatalf("Failed to load certificate: %s\n", err)
+	}
+	caCert, _, err := loadCert("mitm-ca")
+	if err != nil {
+		log.Fatalf("Failed to load CA certificate: %s\n", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AddCert(caCert)
 
 	config = &tls.Config{
 		Certificates: []tls.Certificate{
@@ -70,9 +87,17 @@ func main() {
 				Leaf:       cert,
 			},
 		},
+		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+			fmt.Printf("chains: %v\n", verifiedChains)
+			return nil
+		},
+		InsecureSkipVerify: true,
+		RootCAs:            caCertPool,
+		Renegotiation:      tls.RenegotiateFreelyAsClient,
 	}
+	config.BuildNameToCertificate()
 
-	listener, err := tls.Listen("tcp", ":8443", config)
+	listener, err := tls.Listen("tcp", *listen, config)
 	if err != nil {
 		log.Fatalf("TLS listen failed: %s\n", err)
 	}
@@ -99,8 +124,10 @@ func handleConnection(conn net.Conn, proxy string) {
 	}
 	defer server.Close()
 
-	go copyStream(server, conn, fmt.Sprintf("%s->%s", proxy, conn.RemoteAddr()))
-	copyStream(conn, server, fmt.Sprintf("%s->%s", conn.RemoteAddr(), proxy))
+	go copyStream(conn, server, fmt.Sprintf("Client(%s)->Server(%s)",
+		conn.RemoteAddr(), proxy))
+	copyStream(server, conn, fmt.Sprintf("Server(%s)->Client(%s)",
+		proxy, conn.RemoteAddr()))
 }
 
 func copyStream(from, to net.Conn, name string) {
@@ -108,60 +135,113 @@ func copyStream(from, to net.Conn, name string) {
 	for {
 		n, err := from.Read(buf[:])
 		if err != nil {
+			fmt.Printf("%s: read failed: %s\n", name, err)
 			return
 		}
 		fmt.Printf("%s:\n%s", name, hex.Dump(buf[:n]))
 		n, err = to.Write(buf[:n])
 		if err != nil {
+			fmt.Printf("%s: write failed: %s\n", name, err)
 			return
 		}
 	}
 }
 
-func loadKey(path string) (*rsa.PrivateKey, *x509.Certificate, []byte, error) {
+func loadKey(path string) (*rsa.PrivateKey, error) {
 	keyBytes, err := ioutil.ReadFile(fmt.Sprintf("%s.prv", path))
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, err
 	}
-	key, err := x509.ParsePKCS1PrivateKey(keyBytes)
-	if err != nil {
-		return nil, nil, nil, err
-	}
+	return x509.ParsePKCS1PrivateKey(keyBytes)
+}
+
+func loadCert(path string) (*x509.Certificate, []byte, error) {
 	certBytes, err := ioutil.ReadFile(fmt.Sprintf("%s.crt", path))
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, err
 	}
 	cert, err := x509.ParseCertificate(certBytes)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, err
 	}
-	return key, cert, certBytes, nil
+	return cert, certBytes, nil
 }
 
-func makeKey(path, san string) error {
+func makeKeys() error {
 	key, err := rsa.GenerateKey(rand.Reader, keyBits)
 	if err != nil {
 		return err
 	}
 	keyBytes := x509.MarshalPKCS1PrivateKey(key)
-	err = ioutil.WriteFile(fmt.Sprintf("%s.prv", path), keyBytes, 0600)
+	err = ioutil.WriteFile("mitm-ca.prv", keyBytes, 0600)
 	if err != nil {
 		return err
 	}
 
 	subject := pkix.Name{
-		Organization:       []string{"R&D"},
-		OrganizationalUnit: []string{"CTO Office"},
-		CommonName:         "markkurossi.com",
+		Organization:       []string{"Man-in-the-Middle"},
+		OrganizationalUnit: []string{"CA"},
 	}
 
 	serial, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
 	now := time.Now()
 
+	cert := &x509.Certificate{
+		SerialNumber: serial,
+		Issuer:       subject,
+		Subject:      subject,
+		NotBefore:    now.Add(time.Hour * 24 * -10),
+		NotAfter:     now.Add(time.Hour * 24 * 365 * 5),
+		KeyUsage: x509.KeyUsageDigitalSignature |
+			x509.KeyUsageKeyEncipherment |
+			x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		MaxPathLenZero:        true,
+	}
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, cert,
+		&key.PublicKey, key)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("mitm-ca.crt", certBytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	key, err = rsa.GenerateKey(rand.Reader, keyBits)
+	if err != nil {
+		return err
+	}
+	keyBytes = x509.MarshalPKCS1PrivateKey(key)
+	return ioutil.WriteFile("mitm.prv", keyBytes, 0600)
+}
+
+func makeCert(san string) error {
+	caKey, err := loadKey("mitm-ca")
+	if err != nil {
+		return err
+	}
+	caCert, _, err := loadCert("mitm-ca")
+	if err != nil {
+		return err
+	}
+	key, err := loadKey("mitm")
+	if err != nil {
+		return err
+	}
+
 	var DNSNames []string
 	var IPAddresses []net.IP
 
+	subject := pkix.Name{
+		Organization:       []string{"Man-in-the-Middle"},
+		OrganizationalUnit: []string{"Proxy"},
+	}
+
 	if len(san) > 0 {
+		subject.CommonName = san
+
 		ip := net.ParseIP(san)
 		if ip != nil {
 			IPAddresses = append(IPAddresses, ip)
@@ -170,9 +250,11 @@ func makeKey(path, san string) error {
 		}
 	}
 
+	serial, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+	now := time.Now()
+
 	cert := &x509.Certificate{
 		SerialNumber: serial,
-		Issuer:       subject,
 		Subject:      subject,
 		NotBefore:    now.Add(time.Hour * 24 * -10),
 		NotAfter:     now.Add(time.Hour * 24 * 365 * 5),
@@ -184,10 +266,7 @@ func makeKey(path, san string) error {
 		DNSNames:    DNSNames,
 		IPAddresses: IPAddresses,
 	}
-	certBytes, err := x509.CreateCertificate(rand.Reader, cert, cert,
-		&key.PublicKey, key)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(fmt.Sprintf("%s.crt", path), certBytes, 0644)
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, caCert,
+		&key.PublicKey, caKey)
+	return ioutil.WriteFile("mitm.crt", certBytes, 0644)
 }
